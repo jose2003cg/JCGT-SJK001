52a0bfd3c8a490282a41390a963c78c00b96dc02
joseUji <133038370+jose2003cg@users.noreply.github.com>
2025-11-11 16:39:58 +0100

    Implement PD control with image processing

---
+++ PD.py
@@ -0,0 +1,50 @@
+import WebGUI
+import HAL
+import Frequency
+import cv2
+
+
+KP = 0.0105
+KD = 0.0075 # prueba con este valor y ajusta según comportamiento
+V_MAX = 10.00
+V_MIN = 5.00
+IMG_CENTER = 320
+
+i = 0
+last_err = 0
+
+while True:
+    img = HAL.getImage()
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    red_mask = cv2.inRange(hsv, (0, 125, 125), (30,255,255))
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+
+        if M["m00"] != 0:
+            cX = M["m10"] / M["m00"]
+            err = IMG_CENTER - cX
+
+            # Término derivativo
+            derivative = err - last_err
+
+            # Control PD
+            w_dyn = KP * err + KD * derivative
+
+            v_dyn = V_MAX - ((abs(err) / IMG_CENTER) * (V_MAX - V_MIN))
+
+            HAL.setV(v_dyn)
+            HAL.setW(w_dyn)
+
+            last_err = err  # actualizar error anterior
+        else:
+            HAL.setV(0)
+            HAL.setW(0)
+    else:
+        HAL.setV(0)
+        HAL.setW(0)
+        
+    WebGUI.showImage(img)
+    i += 1
